2781d9e71ca2d648f50c319bb67493cae1b43ca9
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-04-15 02:06:42 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -22,7 +22,7 @@ resource "acme_registration" "reg" {
 
 resource "acme_certificate" "certificate" {
   account_key_pem           = "${acme_registration.reg.account_key_pem}"
-  common_name               = "gatfe.gditsolutiondemo.com"
+  common_name               = "*.gditsolutiondemo.com"
 
   dns_challenge {
     provider = "route53"
